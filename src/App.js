805ff18a4137fe6b2b1520123de72ebe4b8a3d48
globalbmcho <36567660+globalbmcho@users.noreply.github.com>
2021-02-12 11:58:47 +0900

    movie #2.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) {
+    return( <div>
+        <h2>I like {name}</h2>
+          <img width = "300" src = {picture} />
+        </div>
+    );
+}
+
+const foodILike = [
+  {
+    name :"kimchi",
+    image:"https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg"
+  },
+  {
+    name :"galbi",
+    image: "https://www.seriouseats.com/2019/07/20190619-korean-bbq-kalbi-beef-short-ribs-vicky-wasik-21-1500x1125.jpg"
+  },
+  {
+    name :"samgyeopsal",
+    image:"https://i1.wp.com/www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg?fit=900%2C600&ssl=1"
+  },
+  {
+    name :"sushi",
+    image:"https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg"
+  }
+
+
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div> {/*dish is just object*/}
+        {foodILike.map(dish => <Food name = {dish.name} picture = {dish.image}/>)}
+
+    </div>);
 }
 
 export default App;
